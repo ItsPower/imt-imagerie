f46ad38153bbd4de074debd7bdf95878dc423ff2
Its_Power ▼ <34723487+ItsPower@users.noreply.github.com>
2021-10-12 23:33:32 +0200

    Add files via upload

--- main_cam.py
+++ main_cam.py
@@ -1,5 +1,6 @@
 # Imports nécessaires pour ce TP
 import csv
+import os
 
 import args as args
 from imutils import face_utils
@@ -33,8 +34,6 @@ while(True):
     # On applique la detection de visage sur notre image en niveau de gris
     gray_rec = detector(gray, 1)
 
-    print(gray_rec)
-
     # On boucle sur la detection
     for (i, rect) in enumerate(gray_rec):
         # Apres avoir determiner les reperes sur le visage
@@ -49,85 +48,82 @@ while(True):
         # de dlib pour la convertir en rectangle au format opencv
         # Dans le cas on nous ajoutons une rêgle de traitement relatif au différents visages
         # nous pouvons les annoter si plusieurs visages
+        cv2.rectangle(result, (x-10,y-10), (x+w+10,y+h+10), (0,255,0), 1)
+        cv2.putText(result, "face "+str(i), (x, y-15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
 
         # Enfin on boucle sur tout nos coordonnées de points d'interets
         # et on les ajoute à notre image de base avec un petit cercle pour être visible
         j = 0
         landmarks = [None] * 100
         for (x, y) in forme:
-            #print(j)
             landmarks[j] = (x, y)
-
-            print(j,",",x,",",y)
-
-            cv2.circle(result, (x, y), 1, (255, 0, 0), 2)
-
+            #print(j,",",x,",",y)
+            #cv2.circle(result, (x, y), 1, (0, 0, 0), 1)
+            #cv2.putText(result, str(j+1), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,255))
             j = j + 1
 
-        # On enregistre notre résultat de détection
-        #cv2.imwrite("result.jpg", image)
+        for file in os.listdir("masks"):
+            if file.endswith(".csv") and os.path.isfile("masks/"+file.split(".")[0]+".png"):
+                #print("file: "+file+" has csv and image")
 
-        dst_pts = np.array(
-            [
-                forme[48],
-                forme[49],
-                forme[50],
-                forme[51],
-                forme[52],
-                forme[53],
-                forme[54],
-                forme[55],
-                forme[56],
-                forme[57],
-                forme[58],
-                forme[59],
-            ],
-            dtype="float32",
-        )
+                mask_csv = "masks/"+file;
+                mask_image = "masks/"+file.split(".")[0]+".png";
 
-        mask_image = "masks/bouche.png"
-        mask_points = "masks/bouche.csv"
+                with open(mask_csv) as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=",")
+                    tmp_landmarks = []
+                    src_pts = []
+                    dst_translation = []
+                    for i, row in enumerate(csv_reader):
+                        # Si présence de ligne vide alors on passe à la suite
+                        try:
+                            tmp_landmarks.append(forme[int(row[0])-1])
+                            #print(mask_csv,"added landmark ",float(row[0])-1)
+                            src_pts.append(np.array([float(row[1]), float(row[2])]))
+                            #print(mask_csv,"added src_pt ",float(row[1]), float(row[2]))
+                            dst_translation.append((float(row[3]), float(row[4])))
+                            #print(mask_csv,"added dst_translation ",float(row[3]), float(row[4]))
+                        except ValueError:
+                            continue
+                src_pts = np.array(src_pts, dtype="float32")
+                dst_pts = np.array(tmp_landmarks, dtype="float32")
 
-        with open(mask_points) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=",")
-            src_pts = []
-            for i, row in enumerate(csv_reader):
-                # Si présence de ligne vide alors on passe à la suite
-                try:
-                    src_pts.append(np.array([float(row[1]), float(row[2])]))
-                except ValueError:
-                    continue
-        src_pts = np.array(src_pts, dtype="float32")
+                # On applique une superposition si chaque coordonnées à une correspondance
 
-    # On applique une superposition si chaque coordonnées à une correspondance
-    if (len(gray_rec) != 0):
-        if (forme > 0).all():
-            print(">0")
-            # Chargement de l'image
-            mask_img = cv2.imread(mask_image, cv2.IMREAD_UNCHANGED)
-            mask_img = mask_img.astype(np.float32)
-            mask_img = mask_img / 255.0
-            # On créer une matrice de transformation (perspective)
-            M, _ = cv2.findHomography(src_pts, dst_pts)
-            # On applique la transformation
-            transformed_mask = cv2.warpPerspective(
-                mask_img,
-                M,
-                (result.shape[1], result.shape[0]),
-                None,
-                cv2.INTER_LINEAR,
-                cv2.BORDER_CONSTANT,
-            )
-            # cv2.imshow("Image superposée simple", transformed_mask)
-            alpha_mask = transformed_mask[:, :, 3]
-            alpha_image = 1.0 - alpha_mask
-            for c in range(0, 3):
-                result[:, :, c] = (
-                        alpha_mask * transformed_mask[:, :, c]
-                        + alpha_image * result[:, :, c]
-                )
+                if (forme > 0).all():
+                    # NEZ
+                    for idx, val in enumerate(dst_translation):
+                        dst_pts[idx][0] += val[0]
+                        dst_pts[idx][1] += val[1]
 
-    cv2.imshow('frame', result)
+                    mask_img = cv2.imread(mask_image, cv2.IMREAD_UNCHANGED)
+                    mask_img = mask_img.astype(np.float32)
+                    mask_img = mask_img / 255.0
+                    # On créer une matrice de transformation (perspective)
+                    M, _ = cv2.findHomography(src_pts, dst_pts)
+                    # On applique la transformation
+                    transformed_mask = cv2.warpPerspective(
+                        mask_img,
+                        M,
+                        (result.shape[1], result.shape[0]),
+                        None,
+                        cv2.INTER_LINEAR,
+                        cv2.BORDER_CONSTANT,
+                    )
+
+                    alpha_mask = transformed_mask[:, :, 3]
+                    alpha_image = 1.0 - alpha_mask
+                    for c in range(0, 3):
+                        result[:, :, c] = (
+                                alpha_mask * transformed_mask[:, :, c]
+                                + alpha_image * result[:, :, c]
+                        )
+
+                    #cv2.imshow('transformed_mask', transformed_mask)
+                    #cv2.imshow('alpha_mask', alpha_mask)
+                    # END OF NEZ
+
+    cv2.imshow('result', result)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
